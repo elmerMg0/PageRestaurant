5b9957d22cad013fde60f671b96df057b40c47e6
elmerMg0 <mendozagutierrezelmer20@gmail.com>
2022-08-08 17:05:53 -0400

    Bug fixed and changes photos size and whatsapplogo added

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const elementosCarrusel = document.querySelectorAll(".carousel");
     M.Carousel.init(elementosCarrusel,{
         duration: 150,
-        dist: -100,
+        dist: -80,
         shift: 5,
         padding: 5,
         indicators: true,
@@ -20,10 +20,14 @@ document.querySelector(".whole-plate").addEventListener("click",()=>{
         entero = true;
         document.querySelector(".whole-plate").classList.toggle("bg-whole-plate")
         document.querySelector(".middle-plate").classList.toggle("bg-middle-plate")
-        document.querySelector(".menu-dish-txt").classList.toggle("show-menu");
+        document.querySelector(".menu-dish-txt-whole").classList.toggle("show-menu");
         document.querySelector(".menu-dish-txt-middle").classList.toggle("show-menu");
-        document.querySelector(".menu-dish-txt-middle").classList.toggle("tamanio");
-    }
+        
+        document.querySelector(".menu-dish-txt-whole").classList.toggle("margin-bottom");
+        document.querySelector(".menu-dish-txt-middle").classList.toggle("margin-bottom");
+/*      
+   document.querySelector(".menu-dish-txt-middle").classList.toggle("tamanio");
+ */    }
 });
 document.querySelector(".middle-plate").addEventListener("click",()=>{
     if(entero){
@@ -31,8 +35,11 @@ document.querySelector(".middle-plate").addEventListener("click",()=>{
         document.querySelector(".whole-plate").classList.toggle("bg-whole-plate")
         document.querySelector(".middle-plate").classList.toggle("bg-middle-plate")
         document.querySelector(".menu-dish-txt-middle").classList.toggle("show-menu");
-        document.querySelector(".menu-dish-txt").classList.toggle("show-menu");
-        document.querySelector(".menu-dish-txt-middle").classList.toggle("tamanio");
-
+        document.querySelector(".menu-dish-txt-whole").classList.toggle("show-menu");
+        
+        document.querySelector(".menu-dish-txt-middle").classList.toggle("margin-bottom");
+        document.querySelector(".menu-dish-txt-whole").classList.toggle("margin-bottom");
+/*         document.querySelector(".menu-dish-txt-middle").classList.toggle("tamanio");
+ */
     }
 });
